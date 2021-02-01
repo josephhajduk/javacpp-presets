09a09429900c5f45d532d70b975037a001458b4c
Samuel Audet <samuel.audet@gmail.com>
2021-02-01 23:42:00 +0900

    * Upgrade presets for NumPy 1.20.0, ONNX 1.8.1

--- numpy/cppbuild.sh
+++ numpy/cppbuild.sh
@@ -7,7 +7,7 @@ if [[ -z "$PLATFORM" ]]; then
     exit
 fi
 
-NUMPY_VERSION=1.19.5
+NUMPY_VERSION=1.20.0
 download https://github.com/numpy/numpy/releases/download/v$NUMPY_VERSION/numpy-$NUMPY_VERSION.tar.gz numpy-$NUMPY_VERSION.tar.gz
 
 mkdir -p $PLATFORM
